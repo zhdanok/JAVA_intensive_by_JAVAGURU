9a4b58af94c45ccafd4aac29bb489cead0390e39
Liudmila_Zhdanok <zhdanokla@gmail.com>
2021-02-15 00:00:43 +0300

    moved files to another directory

---
+++ 1_MathOperation/src/main/java/io/github/zhdanok/servise/MathOperationDemo.java
@@ -0,0 +1,27 @@
+package io.github.zhdanok.servise;
+
+public class MathOperationDemo {
+
+    public static double addition(double a, double b) {
+        return a + b;
+    }
+
+    public static double multiplication(double c, double d) {
+        return c * d;
+    }
+
+    public static double subtraction(double x, double y) {
+        return x - y;
+    }
+
+    public static double division(double x, double y) {
+        if (y == 0) {
+            return 0;
+        }
+        return x / y;
+    }
+
+    public static double reminder(double x, double y) {
+        return x % y;
+    }
+}
