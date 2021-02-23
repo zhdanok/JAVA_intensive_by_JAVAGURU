ade0235946526a515e1e9e8fe6f6d945faa0e8a9
Liudmila_Zhdanok <zhdanokla@gmail.com>
2021-02-24 00:51:21 +0300

    add task CopyInRange

---
+++ 4_WorkWithArrays/src/main/java/io/github/zhdanok/servise/CopyArray.java
@@ -0,0 +1,27 @@
+package io.github.zhdanok.servise;
+
+public class CopyArray {
+
+    public static int[] copyInRange(int[] example, int a, int b) {
+        int size = 0;
+        for (int x : example) {
+            if (x >= a && x <= b) {
+                size++;
+            }
+        }
+        if (size == 0) {
+            return new int[0];
+        } else {
+            int[] actuale = new int[size];
+            int j = 0;
+            for (int i = 0; i < example.length; i++) {
+                if (example[i] >= a && example[i] <= b) {
+                    actuale[j] = example[i];
+                    j++;
+                }
+            }
+            return actuale;
+        }
+
+    }
+}
