18b67c9f735cff7c498396838dd1fb28aea39a6a
Liudmila_Zhdanok <zhdanokla@gmail.com>
2021-03-11 02:14:38 +0300

    delete catalog

--- 8_Exeptions/src/main/java/io/github/zhdanok/exeptions/UserValidationException.java
+++
@@ -1,7 +0,0 @@
-package io.github.zhdanok.exeptions;
-
-public class UserValidationException extends RuntimeException {
-    public UserValidationException(String message) {
-        super(message);
-    }
-}
